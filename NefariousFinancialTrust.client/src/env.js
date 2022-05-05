7b1971bcf58719e7b1d15729c4023bac3d954046
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2022-05-05 09:30:33 -0600

    what

--- NefariousFinancialTrust.client/src/env.js
+++ NefariousFinancialTrust.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-wglcqf-n.us.auth0.com'
-export const audience = 'https://MarkDev.com'
-export const clientId = 'La0Mv5rjSAdmXsSbXD7g8jUUfVLpdIwM'
+export const domain = 'dev-geue0lhy.us.auth0.com'
+export const audience = 'https://mark-dev.com'
+export const clientId = 'NM80zhquib2VaNIlqci7Zt6tgu7BwUTx'
